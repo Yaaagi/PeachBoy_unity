c1fd01af2681d759f7fd4cfa1c6f6e51a688a47d
YagiAyana <cgjd0310@mail4.doshisha.ac.jp>
2022-01-25 00:31:49 +0900

    unity追加

---
+++ PeachBoy/Assets/Scenes/ToResultScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToResultScene : MonoBehaviour
+{
+    private float step_time;    // 経過時間カウント用
+
+    // Use this for initialization
+    void Start()
+    {
+        step_time = 0.0f;       // 経過時間初期化
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 経過時間をカウント
+        step_time += Time.deltaTime;
+
+        // 5秒後に画面遷移（resultへ移動）
+        if (step_time >= 70.0f)
+        {
+            SceneManager.LoadScene("ResultScene");
+        }
+    }
+}
---
+++ PeachBoy/Assets/UIController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    
+    GameObject gameOverText;
+    GameObject gameFinishText;
+    GameObject gameOverMenu;
+
+    
+
+
+    public void GameOver()
+    {
+        this.gameOverText.GetComponent<Text>().text = "GameOver";
+    }
+
+    public void GameFinish ()
+    {
+        this.gameFinishText.GetComponent<Text>().text = "GameFinish";
+    }
+    public void GameOverMenu ()
+    {
+        gameOverMenu.SetActive(true);
+    }
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameOverText = GameObject.Find("GameOver");
+        this.gameFinishText = GameObject.Find("GameFinish");
+        this.gameOverMenu = GameObject.Find("Panel (1)");
+
+        gameOverMenu.SetActive(false);
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
